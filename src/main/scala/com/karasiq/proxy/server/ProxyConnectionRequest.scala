4d019f5eba957577966d41c166f867f00e98487b
Karasiq <yoba123@yandex.ru>
2018-02-01 14:57:25 +0300

    Fixes

--- src/main/scala/com/karasiq/proxy/server/ProxyConnectionRequest.scala
+++ src/main/scala/com/karasiq/proxy/server/ProxyConnectionRequest.scala
@@ -3,6 +3,7 @@ package com.karasiq.proxy.server
 import java.net.InetSocketAddress
 
 import akka.util.ByteString
+
 import com.karasiq.networkutils.http.HttpStatus
 import com.karasiq.parsers.http.HttpResponse
 import com.karasiq.parsers.socks.SocksClient.SocksVersion.{SocksV4, SocksV5}
@@ -19,7 +20,7 @@ object ProxyConnectionRequest {
       case "https" ⇒
         HttpResponse((HttpStatus(200, "Connection established"), Nil))
 
-      case "socks" ⇒
+      case "socks" | "socks5" ⇒
         ConnectionStatusResponse(SocksV5, None, Codes.success(SocksV5))
 
       case "socks4" ⇒
@@ -35,7 +36,7 @@ object ProxyConnectionRequest {
       case "http" | "https" ⇒
         HttpResponse((HttpStatus(400, "Bad Request"), Nil))
 
-      case "socks" ⇒
+      case "socks" | "socks5" ⇒
         ConnectionStatusResponse(SocksV5, None, Codes.failure(SocksV5))
 
       case "socks4" ⇒
